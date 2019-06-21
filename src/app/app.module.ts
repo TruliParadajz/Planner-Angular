00d8dc11af74867ef30feec6f76a92730277c2bc
Ivan Drulak <ivan.drulak@rathmann.hr>
2019-06-21 14:56:56 +0200

    nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { NavHeaderComponent } from './nav-header/nav-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule,
