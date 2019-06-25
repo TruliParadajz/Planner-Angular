fa6b9e356f67c3e02f3cd2b1269f119c2774cb1f
TruliParadajz <nargz44@gmail.com>
2019-06-25 11:16:52 +0200

    User tasks - v1.0

--- src/app/user-dashboard/user-dashboard.component.ts
+++ src/app/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/User';
+import { Task } from '../Models/Task';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-dashboard.component.css']
 })
 export class UserDashboardComponent implements OnInit {
+  tasks: Task[];
+  user: User;
+  taskFlag = false;
+  dataFetched = false;
 
-  constructor() { }
+  constructor(private service: ApiService) { }
 
   ngOnInit() {
+    this.onLoadDashboard();
+  }
+  
+  ngDoCheck() {
+    if (this.user) {
+      this.dataFetched = true;
+    }
   }
 
+  onLoadDashboard() {
+    const id = +localStorage.getItem('id');
+    this.service.GetUser(id).subscribe(
+      data => {
+        if (data.Tasks.length < 1) {
+          this.taskFlag = true;
+        }
+        this.user = {
+          Id: id,
+          Name: data.Name,
+          Surname: data.Surname,
+          Email: data.Email,
+          Tasks: data.Tasks
+        };
+        this.tasks = data.Tasks;
+        console.log(this.tasks);
+      }
+    );
+  }
 }
