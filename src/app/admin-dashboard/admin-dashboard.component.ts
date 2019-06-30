d6ad508c638f59eeb098a0a2b004148c7b2754c6
TruliParadajz <nargz44@gmail.com>
2019-06-30 17:15:31 +0200

    admin dashboard updated

--- src/app/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin-dashboard/admin-dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { User } from '../Models/User';
 import { Task } from '../Models/Task';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -13,11 +15,21 @@ export class AdminDashboardComponent implements OnInit {
   usersLoaded = false;
   tasks: Task[] = [];
   tasksFlag: boolean[] = [];
+  editFlags: boolean[] = [];
+  taskEditForm: FormGroup;
+  newText: string;
+  newSolved: boolean;
+  date: Date;
+  addTaskFlag: boolean = false;
 
-  constructor(private service: ApiService) { }
+  constructor(private service: ApiService, private router:Router) { }
 
   ngOnInit() {
     this.showUsers();
+    this.taskEditForm = new FormGroup({
+      text: new FormControl('', Validators.compose([Validators.required, Validators.minLength(5), Validators.maxLength(15)])),
+      solved: new FormControl(false)
+    });   
   }
 
 
@@ -57,6 +69,112 @@ export class AdminDashboardComponent implements OnInit {
     );
     console.log(userId, i);
     console.log(this.tasksFlag[i]);
+  }
+
+  editTask(taskId: number, i: number){
+    console.log(this.tasks);
+    this.editFlags[i] = true;
+    this.taskEditForm.patchValue({text: this.tasks[i].Text});
+    this.taskEditForm.patchValue({solved: this.tasks[i].Solved});
+    console.log(this.tasks[i].Date);
+    this.date = new Date(this.tasks[i].Date);
+  }
+
+  saveTask(taskId: number, i: number)
+  {
+    const newText = this.taskEditForm.controls.text.value;
+    const newSolved = this.taskEditForm.controls.solved.value;
+    const newDate = this.date;
+
+    console.log(newDate.toLocaleDateString());
+
+    this.tasks[i].Text = newText;
+    this.tasks[i].Solved = newSolved;
+    this.tasks[i].Date = newDate;
+
+    this.service.UpdateTask(this.tasks[i]).subscribe(
+      () => {
+        console.log('User update');
+      }, error => {
+        if (error.status === 0) {
+          alert('Service is not available, contact your Internet Service Provider!');
+        } else {
+          console.log('Service error: ', error.error.Message);
+          alert(error.error.Message);
+        }
+      }, () => {
+        alert('Task successfully updated!');
+      });
+    this.editFlags[i] = false;
+  }
+
+  deleteTask(taskId: number)
+  {
+    this.service.DeleteTask(taskId).subscribe(
+      () => 
+      {
+        console.log('User update');
+      }, 
+      error => 
+      {
+        if (error.status === 0) 
+        {
+          alert('Service is not available, contact your Internet Service Provider!');
+        } 
+        else 
+        {
+          console.log('Service error: ', error.error.Message);
+          alert(error.error.Message);
+        }
+      }, 
+      () => 
+      {
+        alert('Task successfully deleted!');
+        this.ngOnInit();
+      });
+      
+  }
+  newTask()
+  {
+    this.addTaskFlag = true;
+  }
+  addTask(newText, newSolved, newDate, userId: number)
+  {
+    console.log(newText, newSolved, newDate, userId);
+    const userID = userId;
+
+    this.service.CreateTask(new Task(newText, newSolved, newDate, userID)).subscribe(
+      () => 
+      {
+        console.log('Create task');
+      }, 
+      error => 
+      {
+        if (error.status === 0) 
+        {
+          alert('Service is not available, contact your Internet Service Provider!');
+        } 
+        else 
+        {
+          console.log('Service error: ', error.error.Message);
+          alert(error.error.Message);
+        }
+      }, 
+      () => 
+      {
+        alert('Task successfully created!');
+        this.newText = '';
+        this.newSolved = false;
+        this.service.GetUser(userId).subscribe(
+          data => {
+            this.tasks = data.Tasks;             
+            console.log(this.tasks);
+            console.log("kraj");
+          }
+        );
+      });
+
+      this.addTaskFlag = false;
   }
   
 
